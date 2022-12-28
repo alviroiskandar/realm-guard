ad725e45ca520f670028a45b9c3c44fe87e5e173
Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>
2022-12-28 07:58:04 +0700

    save
    Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>

--- modules/admin/ban.ts
+++ modules/admin/ban.ts
@@ -3,256 +3,114 @@ import { validateRequest } from "../validation";
 import { extract_kick_query } from "./kick";
 import { timeToSecond } from "../generic";
 
-/*
- * TODO(Viro_SSFS,ammarfaizi2):
- *
- *   Refactor this file again, it is confusing to follow.
- *   Sorry for the mess.
- *
- */
+const basic_ban_cmd_rules = [
+        "in_supergroup",
+        "user_is_admin",
+        "bot_is_admin",
+        "noreply_admin",
+];
 
-async function do_ban(ctx: any, user: any, seconds: number = -1,
-                      revoke_msg: boolean = false, silent: boolean = false)
+async function do_ban(ctx: any, user: any)
 {
-        let until_date;
-
-        if (seconds == -1) {
-                /*
-                 * Forever!
-                 */
-                until_date = 0;
-        } else {
-                until_date = Math.floor(Date.now() / 1000) + seconds;
-        }
-
-        await ctx.telegram.banChatMember(ctx.chat.id, user.id, until_date);
-
-        const c = (until_date == 0) ? "" : "T";
-        console.log(`[${c}BAN] ${user.first_name} (${user.id})`);
-
-        if (silent)
-                return;
-
-        let r;
-        if ("time_tban_arg" in ctx)
-                r = `User ${user.first_name} (${user.id}) is temporarily banned for ${ctx.time_tban_arg}!`
-        else
-                r = `User ${user.first_name} (${user.id}) has been banned!`;
-
-        await ctx.reply(r);
+        await ctx.telegram.banChatMember(ctx.chat.id, user.id);
 }
 
-async function do_unban(ctx: any, user: any, silent: boolean = false)
-{
-        await ctx.unbanChatMember(user.id, {only_if_banned: true});
-        console.log(`[UNBAN] ${user.first_name} (${user.id})`);
-
-        if (silent)
-                return;
-
-        await ctx.reply(`User ${user.first_name} (${user.id}) has been unbanned!`);
-}
-
-async function ban_with_reply(ctx: any, seconds: number = -1,
-                              revoke_msg: boolean = false,
-                              silent: boolean = false)
-{
-        await do_ban(ctx, ctx.message.reply_to_message.from, seconds,
-                     revoke_msg, silent);
-}
-
-async function unban_with_reply(ctx: any)
-{
-        await do_unban(ctx, ctx.message.reply_to_message.from);
-}
-
-async function ban_with_user_id(ctx: any, seconds: number = -1,
-                                revoke_msg: boolean = false,
-                                silent: boolean = false)
-{
-        const user = await extract_kick_query(ctx, ctx.message.text);
-
-        if (!user) {
-                if (!ctx.show_resolve_fail_msg)
-                        return false;
-
-                let x = ctx.message.text.split(" ");
-                await ctx.reply(`Cannot resolve user_id ${x[1]}`);
-                return false;
-        }
-
-        await do_ban(ctx, user, seconds, revoke_msg, silent);
-        return true;
-}
-
-async function unban_with_user_id(ctx: any)
-{
-        const user = await extract_kick_query(ctx, ctx.message.text);
-
-        if (!user)
-                return false;
-
-        await do_unban(ctx, user);
-        return true;
-}
-
-async function tban_with_reply(ctx: any, args: string[])
-{
-        ctx.time_tban_arg = args[1];
-        await ban_with_reply(ctx, timeToSecond(args[1]));
-        return true;
-}
-
-async function tban_with_user_id(ctx: any, args: string[])
-{
-        ctx.message.text = `/tban ${args[1]}`;
-        ctx.time_tban_arg = args[2];
-        ctx.show_resolve_fail_msg = true;
-        await ban_with_user_id(ctx, timeToSecond(args[2]));
-        return true;  
-}
-
-async function __ban_cmd(ctx: any, revoke_msg: boolean = false,
-                         silent: boolean = false)
-{
-        const rules = [
-                "in_supergroup",
-                "user_is_admin",
-                "bot_is_admin",
-                "noreply_admin",
-        ];
-
-        if (!(await validateRequest(ctx, rules)))
-                return;
-
-        if (ctx.message?.text) {
-                if (await ban_with_user_id(ctx, -1, revoke_msg, silent))
-                        return;
-        }
-
-        if (ctx.message?.reply_to_message?.from) {
-                await ban_with_reply(ctx, -1, revoke_msg, silent);
-                return;
-        }
-
-        console.log("[ERROR] No valid reply or user ID!");
-        await ctx.reply("Please reply to a message or type the user ID to ban a user!");
-}
-
-async function ban_cmd(ctx: any)
-{
-        await __ban_cmd(ctx, false, false);
-}
-
-async function unban_cmd(ctx: any)
-{
-        const rules = [
-                "in_supergroup",
-                "user_is_admin",
-                "bot_is_admin",
-                "noreply_admin",
-        ];
-
-        if (!(await validateRequest(ctx, rules)))
-                return;
-
-        if (ctx.message?.text) {
-                if (await unban_with_user_id(ctx))
-                        return;
-        }
-
-        if (ctx.message?.reply_to_message?.from) {
-                await unban_with_reply(ctx);
-                return;
-        }
-
-        console.log("[ERROR] No valid reply or user ID!");
-        await ctx.reply("Please reply to a message or type the user ID to unban a user!");
-}
-
-async function send_invalid_tban_notice(ctx: any)
+async function send_ban_cmd_usage(ctx: any)
 {
         const r =
-                `Invalid tban query!\n\n` +
-                `Usage:\n` +
-                `<code>/tban &lt;time&gt;</code>\n` +
-                `<code>/tban &lt;user_id&gt; &lt;time&gt;</code>`
+                `<b>/ban</b> command usage:\n` +
+                `<code>/ban</code> (must reply)\n` +
+                `<code>/ban [reason]</code> (must reply, reason not numeric)\n` +
+                `<code>/ban [user_id]</code>\n` +
+                `<code>/ban [user_id] [reason]</code>`;
 
         await ctx.replyWithHTML(r);
 }
 
-async function tban_cmd(ctx: any)
+async function ban_with_user_id(ctx: any, user_id: number,
+                                reason: string|null = null)
 {
-        const rules = [
-                "in_supergroup",
-                "user_is_admin",
-                "bot_is_admin",
-                "noreply_admin",
-        ];
 
-        if (!(await validateRequest(ctx, rules)))
+}
+
+async function ban_with_reply(ctx: any, reason: string|null = null)
+{
+        const user = ctx.message?.reply_to_message?.from;
+        if (!user) {
+                send_ban_cmd_usage(ctx);
+                return false;
+        }
+
+        await ban_with_user_id(ctx, user.id, reason);
+        return true;
+}
+
+async function parse_ban_arg(text)
+{
+        const args =
+
+        return {
+                user_id: user_id,
+                reason: 
+        };
+}
+
+async function ban_cmd_may_silent()
+{
+        if (!(await validateRequest(ctx, basic_ban_cmd_rules)))
                 return;
 
+        const args = ctx.message.text.split(" ");
+
         /*
-         * Expected values:
-         *
-         *   args[0] = /tban
-         *   args[1] = user_id
-         *   args[2] = time
+         * `/ban` without arguments must reply to a message.
+         */
+        if (args.length == 1) {
+                await ban_with_reply(ctx);
+                return;
+        }
+
+        /*
+         * `/ban` with a single argument, possible scenarios:
+         * 
+         *      args = ["/ban", "reason_single_word"] # (must reply)
          *
          * or
-         *
-         *   args[0] = /tban
-         *   args[1] = time
-         *
+         *      args = ["/ban", "user_id"] # no reason
          */
-        const args = ctx.message.text.split(" ");
-        switch (args.length) {
-        case 2:
-                /*
-                 * `/tban time` needs a replied message.
-                 */
-                if (!(await validateRequest(ctx, ["reply"])))
+        if (args.length == 2) {
+                if (!args[1].match(/^\d+$/)) {
+                        await ban_with_reply(ctx, args[1]);
                         return;
+                } else {
+                        await ban_with_user_id(ctx, Number(args[1]));
+                        return;
+                }
+        }
 
-                await tban_with_reply(ctx, args)
-                break;
-        case 3:
-                await tban_with_user_id(ctx, args);
-                break;
-        default:
-                await send_invalid_tban_notice(ctx);
-                break;
+        /*
+         * `/ban` with a multiple arguments, possible scenarios:
+         * 
+         *      args = ["/ban", "reason1", "reason2", ...] # must reply
+         *
+         * or
+         *      args = ["/ban", "user_id", "reason1", "reason2", ...]
+         */
+        if (!args[1].match(/^\d+$/)) {
+                await ban_with_reply(ctx, args[1]);
+                return;
+        } else {
+                await ban_with_user_id(ctx, Number(args[1]));
+                return;
         }
 }
 
-async function sban_cmd(ctx: any)
+async function ban_cmd(ctx: any)
 {
-        /*
-         * Pretty much the same thing as `/ban` but:
-         * `silent: true`.
-         */
-        await __ban_cmd(ctx, false, true);
+
 }
 
 export const banCommand: Command = {
         command: "ban",
         function: ban_cmd
-};
-
-export const unbanCommand: Command = {
-        command: "unban",
-        function: unban_cmd
-};
-
-// `/tban <time>` command (temporarily ban user)
-export const tbanCommand: Command = {
-        command: "tban",
-        function: tban_cmd
-};
-
-// `/sban` command (silently ban user)
-export const sbanCommand: Command = {
-        command: "sban",
-        function: sban_cmd
 };
